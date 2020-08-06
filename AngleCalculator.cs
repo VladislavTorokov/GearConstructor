be0b1df9d76cdc8e9a7b4a8f506698070b4c1ccc
VladTorokov <vladislav.torokov@gmail.com>
2020-08-07 01:33:53 +0500

    Rework all files.

---
+++ AngleCalculator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GearConsole
+{
+    public static class AngleCalculator
+    {
+        private const double pi = Math.PI;
+        //Рассчет функции инвалюты
+        public static double InvoluteAngle(double inv)
+        {
+            return (Math.Pow(3 * inv, 1 / 3.0) - (2 * inv) / 5.0 + (9 / 175.0) * Math.Pow(3, 2 / 3.0) * Math.Pow(inv, 5 / 3.0) - (2 / 175.0) * Math.Pow(3, 1 / 3.0) * Math.Pow(inv, 7 / 3.0) - (144 / 67375.0) * Math.Pow(inv, 3) + (3258 / 3128125.0) * Math.Pow(3, 2 / 3.0) * Math.Pow(inv, 11 / 3.0) - (49711 / 153278125.0) * Math.Pow(3, 1 / 3.0) * Math.Pow(inv, 13 / 3.0));
+        }
+
+        public static double InvoluteValue(double angle)
+        {
+            return (Math.Tan(angle) - angle);
+        }
+
+        public static double AngleToRadian(double angle)
+        {
+            return angle * pi / 180.0;
+
+        }
+    }
+}
